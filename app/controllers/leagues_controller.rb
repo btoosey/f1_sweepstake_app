069620aff73e1ab570fb43d3956c604925c1dd8b
Ben Toosey <benjamin.toosey@gmail.com>
2022-02-23 09:09:12 +0000

    ADD: Create method working

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,11 +1,26 @@
 class LeaguesController < ApplicationController
-
   def index
     @leagues = League.all
+    @user = current_user if current_user
   end
 
   def new
     @league = League.new
   end
 
+  def create
+    @league = League.new(league_params)
+    @league.user = current_user if current_user
+    if @league.save
+      redirect_to leagues_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def league_params
+    params.require(:league).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'leagues#index'
 
-  resources :leagues, only: [:index, :new, :edit]
+  resources :leagues, only: [:index, :new, :create, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
