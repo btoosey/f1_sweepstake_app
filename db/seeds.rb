c3ec80ca008f026275be77f1978dbbd1b21cab99
Ben Toosey <benjamin.toosey@gmail.com>
2022-02-24 21:19:05 +0000

    Season seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,18 @@
 puts "Generating seeds..."
 
+puts "Generating Seasons..."
+Season.destroy_all
+require_relative "seeds/001_seasons"
+puts "Seasons generated"
+
 puts "Generating Circuits..."
 Circuit.destroy_all
-require_relative "seeds/001_circuits"
+require_relative "seeds/002_circuits"
 puts "Circuits generated"
 
 puts "Generating Constructors..."
 Constructor.destroy_all
-require_relative "seeds/002_constructors"
+require_relative "seeds/003_constructors"
 puts "Constructors generated"
 
 puts "Seeds generated!"
