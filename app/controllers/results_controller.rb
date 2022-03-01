116a55745b8e13abad93844e138ca20a317ae333
Ben Toosey <benjamin.toosey@gmail.com>
2022-03-01 22:36:44 +0000

    ADD: Basic result create method

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,20 @@
 class ResultsController < ApplicationController
+  def new
+    @result = Result.new
+  end
+
+  def create
+    @result = Result.new(result_params)
+    if @result.save
+      redirect_to results_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def result_params
+    params.require(:result).permit(:race_id, :driver_id, :constructor_id, :position)
+  end
 end
