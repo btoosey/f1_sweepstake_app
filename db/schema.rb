d3562edf88b444235829eb1c270c8e8059ace6d5
Ben Toosey <benjamin.toosey@gmail.com>
2022-03-02 20:06:59 +0000

    ADD: Added open? to choices table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_145323) do
+ActiveRecord::Schema.define(version: 2022_03_02_200339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_145323) do
     t.boolean "submitted"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "open"
     t.index ["league_id"], name: "index_choices_on_league_id"
     t.index ["result_id"], name: "index_choices_on_result_id"
     t.index ["user_id"], name: "index_choices_on_user_id"
