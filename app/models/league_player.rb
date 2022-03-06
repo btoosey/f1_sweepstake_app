446446d02872161ebc0cd4806481e0bb9af6e8e9
Ben Toosey <benjamin.toosey@gmail.com>
2022-03-06 23:09:21 +0000

    FIX: Deleted unnecessary league_player files/folders

--- app/models/league_player.rb
+++
@@ -1,6 +0,0 @@
-class LeaguePlayer < ApplicationRecord
-  belongs_to :league
-  belongs_to :user
-
-  validates :league, presence: true, uniqueness: { scope: :user, message: "You have already joined this league" }
-end
