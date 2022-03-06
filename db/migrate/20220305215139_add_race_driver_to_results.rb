0ac6e3157caf279f4535ce20abc3dd41b2d5cfe9
Ben Toosey <benjamin.toosey@gmail.com>
2022-03-06 21:14:59 +0000

    ADD: Add race_driver reference to results table

--- db/migrate/20220305215139_add_race_driver_to_results.rb
+++ db/migrate/20220305215139_add_race_driver_to_results.rb
@@ -1,6 +1,6 @@
 class AddRaceDriverToResults < ActiveRecord::Migration[6.1]
   def change
-    remove_reference :results, :team_driver, foreign_key: true
     add_reference :results, :race_driver, foreign_key: true
+    remove_reference :results, :team_driver, foreign_key: true
   end
 end
