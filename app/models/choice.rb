3095d4ec77c2e0f23f06f11ea28ce056dfcaf5b5
Ben Toosey <benjamin.toosey@gmail.com>
2022-03-23 17:08:31 +0000

    FIX: Corrected user_league/choices associations

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,5 @@
 class Choice < ApplicationRecord
-  belongs_to :league
+  belongs_to :user_league
   belongs_to :result
   belongs_to :user
 end
--- app/models/user_league.rb
+++ app/models/user_league.rb
@@ -2,5 +2,7 @@ class UserLeague < ApplicationRecord
   belongs_to :user
   belongs_to :league
 
+  has_many :choices, dependent: :destroy
+
   validates :league, presence: true, uniqueness: { scope: :user, message: "You have already joined this league" }
 end
