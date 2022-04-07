ecc3b0fd55f01343df02fcda4ec05dfa099c5d6c
Ben Toosey <benjamin.toosey@gmail.com>
2022-04-07 16:42:40 +0100

    ADD: List races for current season on league show page

--- app/models/race.rb
+++ app/models/race.rb
@@ -7,4 +7,18 @@ class Race < ApplicationRecord
   def to_label
     "#{season.year} - #{name}"
   end
+
+  def self.season_race_list(year)
+    season = Season.where(year: year).first
+    races = Race.where(season: season)
+    Race.reorder_races(races)
+  end
+
+  def self.reorder_races(races)
+    races = races.sort_by(&:race_date)
+    races.each_with_index do |race, index|
+      race.round = index + 1
+      race.save
+    end
+  end
 end
