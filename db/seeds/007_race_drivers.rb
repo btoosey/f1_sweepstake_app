f764921d77b315e9a191fae17af8229b45510d23
Ben Toosey <benjamin.toosey@gmail.com>
2022-04-13 16:36:05 +0100

    ADD: Updated race_driver seeds

--- db/seeds/007_race_drivers.rb
+++ db/seeds/007_race_drivers.rb
@@ -43,7 +43,28 @@ race_drivers_list = [
   [2022, race_list[1], "Alfa Romeo F1 Team Orlen", "BOT"],
   [2022, race_list[1], "Alfa Romeo F1 Team Orlen", "ZHO"],
   [2022, race_list[1], "Haas F1 Team", "MAG"],
-  [2022, race_list[1], "Haas F1 Team", "MSC"]
+  [2022, race_list[1], "Haas F1 Team", "MSC"],
+
+  [2022, race_list[2], "Mercedes-AMG Petronas F1 Team", "HAM"],
+  [2022, race_list[2], "Mercedes-AMG Petronas F1 Team", "RUS"],
+  [2022, race_list[2], "Oracle Red Bull Racing", "VER"],
+  [2022, race_list[2], "Oracle Red Bull Racing", "PER"],
+  [2022, race_list[2], "Scuderia Ferrari", "LEC"],
+  [2022, race_list[2], "Scuderia Ferrari", "SAI"],
+  [2022, race_list[2], "McLaren F1 Team", "NOR"],
+  [2022, race_list[2], "McLaren F1 Team", "RIC"],
+  [2022, race_list[2], "BWT Alpine F1 Team", "OCO"],
+  [2022, race_list[2], "BWT Alpine F1 Team", "ALO"],
+  [2022, race_list[2], "Scuderia AlphaTauri", "GAS"],
+  [2022, race_list[2], "Scuderia AlphaTauri", "TSU"],
+  [2022, race_list[2], "Aston Martin Aramco Cognizant F1 Team", "STR"],
+  [2022, race_list[2], "Aston Martin Aramco Cognizant F1 Team", "VET"],
+  [2022, race_list[2], "Williams Racing", "ALB"],
+  [2022, race_list[2], "Williams Racing", "LAT"],
+  [2022, race_list[2], "Alfa Romeo F1 Team Orlen", "BOT"],
+  [2022, race_list[2], "Alfa Romeo F1 Team Orlen", "ZHO"],
+  [2022, race_list[2], "Haas F1 Team", "MAG"],
+  [2022, race_list[2], "Haas F1 Team", "MSC"]
 ]
 
 race_drivers_list.each do |details|
