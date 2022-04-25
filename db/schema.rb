9f60533b7e98f46a1cf60edfe824643f93a291af
Ben Toosey <benjamin.toosey@gmail.com>
2022-04-25 15:43:12 +0100

    FIX: Changed race_date to DateTime datatype and updated times

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_23_223524) do
+ActiveRecord::Schema.define(version: 2022_04_25_142312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2022_04_23_223524) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.date "race_date"
+    t.datetime "race_date"
     t.index ["circuit_id"], name: "index_races_on_circuit_id"
     t.index ["season_id"], name: "index_races_on_season_id"
   end
