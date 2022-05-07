cefee990318e00341242ad15dee6d783d44f4d31
Ben Toosey <benjamin.toosey@gmail.com>
2022-05-07 15:33:40 +0100

    ADD: Random choice generator added

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,11 @@
 class Choice < ApplicationRecord
   belongs_to :user_league
   belongs_to :race_driver
+
+  def self.generate_random_choice(race, user_league)
+    race_drivers = RaceDriver.where(race: race)
+    random_pick = rand(0..19)
+    randomly_driver = race_drivers[random_pick]
+    Choice.create(user_league: user_league, race_driver: randomly_driver)
+  end
 end
---
+++ test/jobs/randomize_unchosen_choices_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RandomizeUnchosenChoicesJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
