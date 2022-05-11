500b6857864f2bcae68321b74787e387d3763490
Ben Toosey <benjamin.toosey@gmail.com>
2022-05-11 09:56:37 +0100

    ADD: Pundit policy for drivers

---
+++ app/policies/driver_policy.rb
@@ -0,0 +1,22 @@
+class DriverPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.role == "admin"
+        scope.all
+      else
+        raise Pundit::NotAuthorizedError, "Not authorized to view this content"
+      end
+    end
+  end
+
+  def index?
+  end
+
+  def create?
+    user.role == "admin"
+  end
+
+  def destroy?
+    user.role == "admin"
+  end
+end
