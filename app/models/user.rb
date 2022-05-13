27119a01482fbbcff60acf2f68e24a1549a16c4c
Ben Toosey <benjamin.toosey@gmail.com>
2022-05-13 15:59:35 +0100

    ADD: Username added to users, with ability to log in with either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,35 @@ class User < ApplicationRecord
 
   has_many :user_leagues, dependent: :destroy
 
+  attr_writer :login
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
   enum role: [:user, :admin]
 
   after_initialize :set_default_role, :if => :new_record?
 
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
   def set_default_role
     self.role ||= :user
+  end
+
+  def login
+    @login || self.username || self.email
   end
 end
